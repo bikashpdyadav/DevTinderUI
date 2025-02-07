f36cf8c571f74cc1939c59b2c4619e6f3dcffbdd
bikashpdyadav <bikashpdyadav11@gmail.com>
2025-02-08 04:58:01 +0530

    Built dynamic user card on feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { BASE_URL } from '../utils/constants';
+import { useDispatch, useSelector } from 'react-redux';
+import { addFeed } from '../utils/feedSlice';
+import UserCard from './UserCard';
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
+  const dispatch = useDispatch();
+  const feed = useSelector((store) => store.feed);
+
+  const getFeed = async () => {
+    if(feed) return;
+    try{
+      const res = await axios.get(BASE_URL+"/feed", {withCredentials: true});
+      dispatch(addFeed(res?.data));
+    }
+    catch(err){
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+  return feed && (
+    <div className='flex justify-center items-center'>
+      <UserCard user={feed[0]} />
+    </div>
   )
 }
 
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const UserCard = ({ user }) => {
+    const { firstName, lastName, about, photoUrl, skills, age } = user;
+    return (
+        <div className="card bg-base-300 w-96 shadow-xl">
+            <figure>
+                <img
+                    src={photoUrl || "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"}
+                    alt="Shoes" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{firstName+" "+lastName}</h2>
+                <p>{about}</p>
+                {age && gender && <p>{age+", "+gender}</p>}
+                <div className="card-actions justify-between">
+                    <button className="btn btn-primary">Ignore</button>
+                    <button className="btn btn-secondary">Interested</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserCard
